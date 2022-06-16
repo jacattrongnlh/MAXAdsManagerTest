d0203e36b39fdfb13d2b687816fb5d0bb53a0eb9
nguyentrong101094 <nguyentrong101094@gmail.com>
2022-06-16 17:21:51 +0700

    method & button to show mediation debugger

---
+++ Scripts/DebugMAXButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Omnilatent.AdsMediation.MAXWrapper
+{
+    public class DebugMAXButton : MonoBehaviour
+    {
+        public void ShowMediationDebugger()
+        {
+            MAXAdsWrapper.ShowMediationDebugger();
+        }
+    }
+}
